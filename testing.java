b2cf4bd78b490b2a4a322feef6ecadc62bb22d69
Cristian Herrera <71306944+cristiandherrera@users.noreply.github.com>
2022-10-03 20:06:44 -0700

    Update testing.java
    added comment

--- testing.java
+++ testing.java
@@ -2,9 +2,10 @@
 
 public class testing {
   public static void main(String[] args) {
+    // EVERYONE PULL FROM THE GITHUB AND PUSH YOUR OWN PRINT STATEMENT W/ YOUR NAME!!!
+
     System.out.println("test");
     System.out.println("test2");
-  
     System.out.println("My name Cristian Herrera and I am a 10x developer that borrows all there code from stack overflow");
   }
 
