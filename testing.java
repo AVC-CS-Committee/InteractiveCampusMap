8c6094b0636b37e37f4d35ed3e72f117c6911998
Cristian Herrera <71306944+cristiandherrera@users.noreply.github.com>
2022-10-03 20:04:31 -0700

    Update testing.java
    added class and main method for a program that can run...

--- testing.java
+++ testing.java
@@ -1,6 +1,14 @@
 //Testing
-System.out.print("test");
-System.out.print("test2");
+
+public class testing {
+  public static void main(String[] args) {
+    System.out.println("test");
+    System.out.println("test2");
+  
+    System.out.println("My name Cristian Herrera and I am a 10x developer that borrows all there code from stack overflow");
+  }
 
 
-System.out.print("My name Cristian Herrera and I am a 10x developer that borrows all there code from stack overflow");
+}
+
+
