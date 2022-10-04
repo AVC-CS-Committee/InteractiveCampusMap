450db938dcd8dbdf6bf7f31778151cb68476cc3d
Cristian Herrera <71306944+cristiandherrera@users.noreply.github.com>
2022-10-03 19:06:59 -0700

    Update testing.java
    who am I?

--- testing.java
+++ testing.java
@@ -1,9 +1,6 @@
 //Testing
 System.out.print("test");
 System.out.print("test2");
-System.out.print("I am hack. I steal all my code from stack overflow");
-
-System.our.print("JK I AM A 10X developer...");
 
 
-System.out.print("Interactive map here we come");
+System.out.print("My name Cristian Herrera and I am a 10x developer that borrows all there code from stack overflow");
