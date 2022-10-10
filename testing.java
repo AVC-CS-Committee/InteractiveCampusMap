e2b811e5cb0c3d5e5617231405cb169cafb2b0d5
Cristian Herrera <71306944+cristiandherrera@users.noreply.github.com>
2022-10-10 15:41:54 -0700

    did some stuff... thats specific enough right?

--- testing.java
+++ testing.java
@@ -1,12 +1,23 @@
 //Testing
 
 public class testing {
+
   public static void main(String[] args) {
     // EVERYONE PULL FROM THE GITHUB REPO AND PUSH YOUR OWN PRINT STATEMENT W/ YOUR NAME!!!
 
     System.out.println("test");
     System.out.println("test2");
-    System.out.println("My name Cristian Herrera and I am a 10x developer that borrows all there code from stack overflow");
+    System.out.println("My name Cristian Herrera and I am a 10x developer that \"borrows\" all there code from stack overflow");
+
+    someonePlsCommit();
+  }
+
+  public static void someonePlsCommit() {
+    // CHANGE ME TO FALSE
+    boolean noOneHasCommited = false;
+
+    if (noOneHasCommited) System.out.println("Why hasnt ANYONE commited yet?!?!");
+    else System.out.println("WE DID IT!! wE 10x deVs nOw boIs! USA! USA! USA! USA!");
   }
 
 
