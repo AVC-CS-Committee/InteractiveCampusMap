161f79ab4fc8700c7bdfe44ef36ea40a18ac105c
nhoss <70042966+nhoss@users.noreply.github.com>
2022-10-16 16:18:33 -0700

    Update testing.java

--- testing.java
+++ testing.java
@@ -1,4 +1,21 @@
 //Testing
-System.out.print("test");
-System.out.print("test2");
-System.out.print("tyler noob");
+public static void main(String[] args) {
+    // EVERYONE PULL FROM THE GITHUB REPO AND PUSH YOUR OWN PRINT STATEMENT W/ YOUR NAME!!!
+
+    System.out.println("test");
+    System.out.println("test2");
+    System.out.println("Shane is C00l, but yall are c00ler 0-0");
+    System.out.println("My name Cristian Herrera and I am a 10x developer that \"borrows\" all there code from stack overflow");
+
+    someonePlsCommit();
+
+    System.out.println("tyler did big oof, but lawyer fixing");
+  }
+
+  public static void someonePlsCommit() {
+    // CHANGE ME TO FALSE
+    boolean noOneHasCommited = true;
+
+    if (noOneHasCommited) System.out.println("Why hasnt ANYONE commited yet?!?!");
+    else System.out.println("WE DID IT!! wE 10x deVs nOw boIs! USA! USA! USA! USA!");
+  }
