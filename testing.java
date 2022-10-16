159d53529f9821e520b00e66d1ec2e132e8f660b
tcartermills <114964572+tcartermills@users.noreply.github.com>
2022-10-16 15:57:47 -0700

    Tyler's pull test

--- testing.java
+++ testing.java
@@ -1,26 +1,3 @@
 //Testing
-
-public class testing {
-
-  public static void main(String[] args) {
-    // EVERYONE PULL FROM THE GITHUB REPO AND PUSH YOUR OWN PRINT STATEMENT W/ YOUR NAME!!!
-
-    System.out.println("test");
-    System.out.println("test2");
-    System.out.println("Shane is C00l, but yall are c00ler 0-0");
-    System.out.println("My name Cristian Herrera and I am a 10x developer that \"borrows\" all there code from stack overflow");
-
-    someonePlsCommit();
-  }
-
-  public static void someonePlsCommit() {
-    // CHANGE ME TO FALSE
-    boolean noOneHasCommited = true;
-
-    if (noOneHasCommited) System.out.println("Why hasnt ANYONE commited yet?!?!");
-    else System.out.println("WE DID IT!! wE 10x deVs nOw boIs! USA! USA! USA! USA!");
-  }
-
-}
-
-
+System.out.print("test");
+System.out.print("test2");
