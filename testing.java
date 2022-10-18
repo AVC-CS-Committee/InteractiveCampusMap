4e3f6792908dcceb5b8795f86459720ece938ae7
Nhat Nguyen (Conan) <nhatn1412@gmail.com>
2022-10-17 18:03:23 -0700

    conan testing

--- testing.java
+++ testing.java
@@ -25,4 +25,5 @@ public static void main(String[] args) {
     System.out.println("Error");
 
     System.out.println("This is a random line!");
+    System.out.println("Another testing from Conan");
   }
