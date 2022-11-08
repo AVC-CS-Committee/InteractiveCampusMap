e0e0ab1171adeb65840ef1976f7cb0bfb9c1d9f6
KallosP <97070073+KallosP@users.noreply.github.com>
2022-11-07 19:11:02 -0800

    Comments, Dependency Update, and Cleanup
    -Added TODO comment in MainActivity.java
    
    -Added Google Services dependency for Maps to the gradle
    
    -Removed the Hello World textView
    
    -NOTE: Not too sure why line 3 was added to the gradle.xml file (let me know if it causes issues)

--- app/src/main/java/com/example/avcinteractivemapapp/MainActivity.java
+++ app/src/main/java/com/example/avcinteractivemapapp/MainActivity.java
@@ -4,6 +4,20 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+/*TODO: (FOR THOSE WORKING ON THE GOOGLE MAPS API)
+        Tutorial being followed: https://youtu.be/lBW58tPLn-A?list=PLgCYzUzKIBE-SZUrVOsbYMzH7tPigT3gi
+        -Add Google Maps Dependency to Gradle file (DONE)
+        -Get a working API Key
+        -Restrict API Key; get done before deployment
+        -Add API key to the manifest file
+        -Add permissions ? (11:00 in tutorial)
+        -Look into Google Services, GPS, and location permissions (https://youtu.be/1f4b2-Y_q2A?list=PLgCYzUzKIBE-SZUrVOsbYMzH7tPigT3gi)
+        -Display a Google Map using a MapView; customize (make it look like the final design)
+        -...
+
+ */
+
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
