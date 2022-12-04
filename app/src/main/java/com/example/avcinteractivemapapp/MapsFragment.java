9b60f2def74833f9fed999c2db8680edc31c08ac
KallosP <97070073+KallosP@users.noreply.github.com>
2022-12-03 19:11:50 -0800

    Revert "Update MapsFragment.java"
    This reverts commit 29a2d6cadc9687b98a174aa5c9fc528473ae8d28.

--- app/src/main/java/com/example/avcinteractivemapapp/MapsFragment.java
+++ app/src/main/java/com/example/avcinteractivemapapp/MapsFragment.java
@@ -677,6 +677,5 @@ public class MapsFragment extends Fragment {
 
         // after generating our bitmap we are returning our bitmap.
         return BitmapDescriptorFactory.fromBitmap(bitmap);
-        //nasif edited here
     }
 }
