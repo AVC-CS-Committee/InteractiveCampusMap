0497c8b34c757ccb5451c199f6399da8c3681f6b
KallosP <97070073+KallosP@users.noreply.github.com>
2023-01-28 15:33:50 -0800

    Added Toolbar and Back button to Support Activity

--- app/src/main/java/com/example/avcinteractivemapapp/ContactActivity.java
+++ app/src/main/java/com/example/avcinteractivemapapp/ContactActivity.java
@@ -1,8 +1,11 @@
 package com.example.avcinteractivemapapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.content.res.AppCompatResources;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class ContactActivity extends AppCompatActivity {
 
@@ -10,5 +13,16 @@ public class ContactActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        //setSupportActionBar(toolbar);
+        toolbar.setNavigationIcon(AppCompatResources.getDrawable(ContactActivity.this, R.drawable.back_icon));
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
+
     }
 }
