0697e2baa5a95dd071cdef2bba9c3eca4e896575
KallosP <97070073+KallosP@users.noreply.github.com>
2023-02-15 23:19:21 -0800

    Implemented Image Flipping for Locations
    - Images for each location are flipped through if that location has more than one
    
    - Currently only supports up to 3 images per location
    
    - Set images of uhazyhall and ssv to have random images for testing purposes, should be changed to correct images
    
    - Need to implement some sort of animations, images currently flip too abruptly, doesn't look clean

--- app/src/main/java/com/example/avcinteractivemapapp/TemplateMarkerDescriptions.java
+++ app/src/main/java/com/example/avcinteractivemapapp/TemplateMarkerDescriptions.java
@@ -5,8 +5,10 @@ import androidx.appcompat.content.res.AppCompatResources;
 import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.ViewFlipper;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -15,11 +17,14 @@ import java.util.ArrayList;
 
 public class TemplateMarkerDescriptions extends AppCompatActivity {
     ArrayList<String> imagePaths = new ArrayList<>();
+    ViewFlipper  viewFlipper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_template_marker_descriptions);
+
+        viewFlipper = (ViewFlipper) findViewById(R.id.imageScroller); // get the reference of ViewFlipper
 
         // Adds the back button
         Toolbar toolbar = findViewById(R.id.toolbar);
@@ -57,9 +62,53 @@ public class TemplateMarkerDescriptions extends AppCompatActivity {
 
             if (imageId == 0) return;
 
-            // Set image
-            ImageView image = findViewById(R.id.location_imageView);
+            // Sets how quickly the images are flipped (in milliseconds)
+            viewFlipper.setFlipInterval(1000);
+
+            // The 1st image is always set
+            ImageView image = findViewById(R.id.image1);
             image.setImageResource(imageId);
+
+            // Set image(s)
+            // True if location has 3 images
+            if(imagePaths.size() == 3) {
+
+                // Starts flipping through images
+                viewFlipper.startFlipping();
+
+                // Set image 2
+                int imageId2 = getResources().getIdentifier(imagePaths.get(1), "drawable", getPackageName());
+                ImageView image2 = findViewById(R.id.image2);
+                image2.setImageResource(imageId2);
+
+                // Set image 3
+                int imageId3 = getResources().getIdentifier(imagePaths.get(2), "drawable", getPackageName());
+                ImageView image3 = findViewById(R.id.image3);
+                image3.setImageResource(imageId3);
+
+            }
+            // True if location has 2 images
+            else if(imagePaths.size() == 2) {
+
+                // Removes the last template image since only 2 images are being flipped through
+                viewFlipper.removeView(findViewById(R.id.image3));
+
+                // Starts flipping through images
+                viewFlipper.startFlipping();
+
+                // Set image 2
+                int imageId2 = getResources().getIdentifier(imagePaths.get(1), "drawable", getPackageName());
+                ImageView image2 = findViewById(R.id.image2);
+                image2.setImageResource(imageId2);
+
+            }
+            // Default case is just 1 image
+            else {
+
+                // Stop flipping if only 1 image (otherwise would flip through template images)
+                viewFlipper.stopFlipping();
+            }
+
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -77,4 +126,6 @@ public class TemplateMarkerDescriptions extends AppCompatActivity {
             throw new RuntimeException(e);
         }
     }
+
+
 }
